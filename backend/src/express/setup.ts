3dc7b11804ba5b6cdeff396d5d60839c4209d3ba
Woyte <t.woityschyn.github@outlook.de>
2023-10-08 18:48:03 +0200

    fix build

--- backend/src/express/setup.ts
+++ backend/src/express/setup.ts
@@ -8,12 +8,14 @@ export const server = createServer(app);
 export function startServer() {
     app.use(text());
     app.use(json());
-    app.use(cors())
-    process.once("SIGTERM", stopServer);
+    app.use(cors());
+    process.on("SIGTERM", stopServer);
     server.listen(3000, () => console.log(`server listening on port ${3000}.`));
 }
 
 export function stopServer() {
+    process.off("SIGTERM", stopServer);
+
     if (server.listening) {
         server.close(() => {});
         console.log("server closed");
