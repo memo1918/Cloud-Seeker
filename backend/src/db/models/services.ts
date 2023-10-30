80f5750a43054b37a01357175139baed49c794ed
Timo Woityschyn <woityschyn@stud.hs-heilbronn.de>
2023-10-30 17:04:53 +0100

    added implementation for downloading infracost db dump and importing that into our own db

--- backend/src/db/models/services.ts
+++ backend/src/db/models/services.ts
@@ -5,9 +5,31 @@ export interface IServices extends WithId<Document> {
     name: string;
 }
 
+const dbName = "cloud-seeker";
+const collectionName = "services";
 
-export async function findServices(client: MongoClient, sku:string[])  {
-    let serviceCollection = await getCollection(client, "cloud-seeker", "services");
-    return serviceCollection.find({ sku: {$in: sku} }).toArray();
+export async function findServices(client: MongoClient, sku: string[]) {
+    let serviceCollection = await getCollection(client, dbName, collectionName);
+    return serviceCollection.find({ sku: { $in: sku } }).toArray();
 }
 
+export async function dropServices(client: MongoClient) {
+    return client.db(dbName).dropCollection(collectionName);
+}
+
+export async function createServicesIndex(client: MongoClient) {
+    let serviceCollection = await getCollection(client, dbName, collectionName);
+    return serviceCollection.createIndex({
+        sku: "text"
+    });
+}
+
+export async function insertServicesData(client: MongoClient, services: any[]) {
+    let serviceCollection = await getCollection(client, dbName, collectionName);
+    return serviceCollection.insertMany(services);
+}
+
+export async function getUniqueVendors(client: MongoClient) {
+    let serviceCollection = await getCollection(client, dbName, collectionName);
+    return serviceCollection.distinct("vendorName");
+}
