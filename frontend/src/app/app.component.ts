8330a1d490a772654552247ab5cde2886f46a783
Timo Woityschyn <woityschyn@stud.hs-heilbronn.de>
2023-11-22 17:13:11 +0100

    added custom routing service

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import {Component} from '@angular/core';
-import {APIService} from "./api.service";
-import {CategoryService} from "./category/category.service";
+import { Component } from "@angular/core";
+import { APIService } from "./api.service";
+import { CategoryService } from "./category/category.service";
+import { RoutingService } from "./routing.service";
 
 @Component({
     selector: 'app-root',
@@ -10,7 +11,7 @@ import {CategoryService} from "./category/category.service";
 export class AppComponent {
     title: string = 'frontend';
 
-    constructor(public api: APIService, public categoryService: CategoryService) {
+  constructor(public api: APIService, public categoryService: CategoryService, public routingService: RoutingService) {
         this.api.loadCounter();
     }
 }
