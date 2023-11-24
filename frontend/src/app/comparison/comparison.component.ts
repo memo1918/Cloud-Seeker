e1f72ba6594ca0e635edeb4aff33070400a26dbc
jhaufer <jhaufer@stud.hs-heilbronn.de>
2023-11-24 12:56:04 +0100

    fix cart item

--- frontend/src/app/comparison/comparison.component.ts
+++ frontend/src/app/comparison/comparison.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import {MatExpansionModule} from '@angular/material/expansion';
 import { ShoppingCartService } from "../shopping-cart.service";
 import {InstanceComparison} from "../models/instance-comparison";
-
+import {valuesIn} from "lodash";
+import {CartItem} from "../models/cart-item";
 
 @Component({
   selector: 'app-comparison',
@@ -12,29 +13,34 @@ import {InstanceComparison} from "../models/instance-comparison";
 
 export class ComparisonComponent {
   panelOpenState: boolean =false;
-  selectedOption: string[] = []; // Initialize it with the default value
+  // selectedOption: string[] = []; // Initialize it with the default value
   constructor(public shoppingCart:ShoppingCartService) {
     this.shoppingCart.getItemsObserver().subscribe(items=>this.newShoppingCartItems(items))
   }
   protected readonly keys = Object.keys;
-  private newShoppingCartItems(items: InstanceComparison[]){
+  private newShoppingCartItems(items: CartItem[]){
     if(items.length == 0){
       return;
     }
-    this.vendors = Object.keys(items[0].price);
+    this.vendors = Object.keys(items[0].instance.price);
     this.columnsToDisplay = ["name", ...this.vendors];
-    for (let i = 0; i < items.length; i++) {
-      const item = items[i];
-      let lowestPrice: number = Number.POSITIVE_INFINITY;
-      let lowestVendor: string = "";
-      for (const vendor of this.vendors) {
-        if (+item.price[vendor].value < lowestPrice){
-          lowestVendor = vendor;
-          lowestPrice = +item.price[vendor].value;
-        }
-      }
-      this.selectedOption[i] = lowestVendor;
-    }
+    // for (let i = 0; i < items.length; i++) {
+    //   const item = items[i];
+    //   let lowestPrice: number = Number.POSITIVE_INFINITY;
+    //   let lowestVendor: string = "";
+      // for (const vendor of this.vendors) {
+      //   let value = item.instance.price[vendor].value;
+      //   if (typeof value == "string") {
+      //     value = value.replace(/\D/g,'')
+      //   }
+      //   let valueN: number = value as number;
+      //   // if (valueN < lowestPrice){
+      //   //   lowestVendor = vendor;
+      //   //   lowestPrice = +item.price[vendor].value;
+      //   // }
+      // }
+      // this.selectedOption[i] = lowestVendor;
+    // }
   }
   vendors: string[] = []
   columnsToDisplay: string[] = [];
@@ -42,11 +48,18 @@ export class ComparisonComponent {
   getTotalPrice(){
     const cartItems = this.shoppingCart.getItems();
     let endPrice = 0;
-      for (let i = 0; i < this.selectedOption.length; i++) {
-      const selected = this.selectedOption[i];
-      const item = cartItems[i];
-      endPrice += +item.price[selected].value;
+    for (const cartItem of cartItems) {
+
+      let price = cartItem.pricingInformation[cartItem.selectedProvider];
+
+      endPrice += price.price;
     }
+    //   for (let i = 0; i < this.selectedOption.length; i++) {
+    //   const selected = this.selectedOption[i];
+    //   const item = cartItems[i];
+    // }
       return endPrice;
   }
+
+  protected readonly valuesIn = valuesIn;
 }
--- frontend/src/app/instance-configuration/dialog/dialog.component.ts
+++ frontend/src/app/instance-configuration/dialog/dialog.component.ts
@@ -1,8 +1,11 @@
-import { Component } from "@angular/core";
-import { MatDialog, MatDialogRef } from "@angular/material/dialog";
-import { InstanceConfigurationComponent } from "../instance-configuration.component";
-import { INSTANCE_COMPARISON_FIXTURE } from "../../fixtures/instance-comparison.fixture";
-import { InstanceComparison } from "src/app/models/instance-comparison";
+import {Component} from "@angular/core";
+import {MatDialog, MatDialogRef} from "@angular/material/dialog";
+import {InstanceConfigurationComponent} from "../instance-configuration.component";
+import {INSTANCE_COMPARISON_FIXTURE} from "../../fixtures/instance-comparison.fixture";
+import {InstanceComparison} from "src/app/models/instance-comparison";
+import {CartItem} from "../../models/cart-item";
+import {ShoppingCartService} from "../../shopping-cart.service";
+import {Unit} from "../../pricing/unit";
 
 @Component({
   selector: "app-instance-configuration-dialog",
@@ -13,13 +16,13 @@ export class DialogComponent {
 
   public dialogRef: MatDialogRef<InstanceConfigurationComponent, any> | undefined;
 
-  constructor(public dialog: MatDialog) {
+  constructor(public dialog: MatDialog, public shoppingCart: ShoppingCartService) {
   }
 
   openDialog(instance: InstanceComparison) {
     return new Promise<MatDialogRef<InstanceConfigurationComponent, any>>((resolve) => {
       const dialogRef = this.dialog.open(InstanceConfigurationComponent, {
-        data: { instance }
+        data: {instance}
       });
       this.dialogRef = dialogRef;
 
@@ -27,8 +30,36 @@ export class DialogComponent {
         resolve(dialogRef);
       });
 
-      this.dialogRef.afterClosed().subscribe(result => {
+      this.dialogRef.afterClosed().subscribe((result) => {
+
         console.log("The dialog was closed", result);
+
+        let pricing = result.adjustedPricing as { providerName: string, factor: number, price: number }[]
+        let units = result.units as {
+          configuration: Unit;
+          providerName: string;
+          providerDefault: Unit;
+        }
+
+        let cartItem: CartItem = {
+          instance: instance,
+          pricingInformation: {},
+          units: units,
+          selectedProvider: "aws",
+          notes: result.notes
+        }
+
+        for (const pricingElement of pricing) {
+          cartItem.pricingInformation[pricingElement.providerName] = {
+            factor: pricingElement.factor,
+            price: pricingElement.price
+          };
+        }
+
+        let items = this.shoppingCart.getItems()
+        items.push(cartItem);
+        this.shoppingCart.setItems(items);
+
         this.dialogRef = undefined;
       });
     });
