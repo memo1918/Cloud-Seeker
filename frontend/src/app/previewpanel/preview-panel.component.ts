592c040017998da5adf41361dfa0c3f36dcda7a0
mueces <mueces@stud.hs-heilbronn.de>
2023-11-24 15:49:35 +0100

    sorting and groupping of the cartitems

--- frontend/src/app/previewpanel/preview-panel.component.ts
+++ frontend/src/app/previewpanel/preview-panel.component.ts
@@ -1,32 +1,62 @@
-import { Component, OnChanges, OnInit,SimpleChanges } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { HeaderService } from "../header/header.service";
 import { ShoppingCartService } from "../shopping-cart.service";
-import { Subscription } from "rxjs";
-import { InstanceComparison } from "../models/instance-comparison";
+import { CartItem } from "../models/cart-item";
+import { CartItemGroup } from "../models/cart-item-group";
 
 
 @Component({
-  selector: 'app-previewpanel',
-  templateUrl: './preview-panel.component.html',
-  styleUrls: ['./preview-panel.component.scss']
+  selector: "app-previewpanel",
+  templateUrl: "./preview-panel.component.html",
+  styleUrls: ["./preview-panel.component.scss"]
 })
 export class PreviewPanelComponent implements OnInit {
   showPanel = false;
-  items: InstanceComparison[] = [];
 
-  constructor(private headerService: HeaderService, private shoppingCart: ShoppingCartService) {
+  constructor(private headerService: HeaderService, public shoppingCart: ShoppingCartService) {
   }
 
   ngOnInit() {
     this.headerService.toggle$.subscribe((toggle) => (this.showPanel = toggle));
-    this.shoppingCart.items$.subscribe((itemList)=> {
-      this.items = itemList;
-      console.log(this.items.length);
-    });
+    // this.shoppingCart.getItemsObserver().subscribe((items) => (this.items$ = items));
   }
 
   public close() {
     this.showPanel = false; // Always set to false when closing
+  }
+
+  sortIntoGroups(list: CartItem[] | null): CartItemGroup[] {
+    if (list == null) {
+      list = [];
+    }
+    list = list.sort((a, b) => a.instance.categoryName.localeCompare(b.instance.categoryName));
+    let current: CartItemGroup | undefined;
+    let result: CartItemGroup[] = [];
+
+    for (let cartItem of list) {
+      if (!current) {
+        current = {
+          category: cartItem.instance.categoryName,
+          items: []
+        };
+        result.push(current)
+      }
+
+      if(current.category == cartItem.instance.categoryName){
+        current.items.push(cartItem)
+      }
+
+      else {
+        current = {
+          category: cartItem.instance.categoryName,
+          items: [cartItem]
+        };
+        result.push(current)
+      }
+
+    }
+    return result;
+
   }
 
 }
