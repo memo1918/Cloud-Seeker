b615235dbf031d0c7ed0c0b8e7e482cb8debf0fb
KaiMittendorff <kmittendor@stud.hs-heilbronn.de>
2023-11-27 15:28:06 +0100

    finished styling of Instance Preview Frontend

--- frontend/src/app/instance-preview/instance-preview.component.ts
+++ frontend/src/app/instance-preview/instance-preview.component.ts
@@ -1,28 +1,32 @@
-import {Component} from "@angular/core";
-import {InstancePreviewService} from "./instance-preview.service";
-import {toPairs, valuesIn} from "lodash";
-import {CategoryService} from "../category/category.service";
-import {FilterService} from "../filter/filter.service";
+import { Component } from "@angular/core";
+import { InstancePreviewService } from "./instance-preview.service";
+import { toPairs, valuesIn } from "lodash";
+import { CategoryService } from "../category/category.service";
+import { FilterService } from "../filter/filter.service";
 
 
 @Component({
-    selector: "app-instance-preview",
-    templateUrl: "./instance-preview.component.html",
-    styleUrls: ["./instance-preview.component.scss"]
+  selector: "app-instance-preview",
+  templateUrl: "./instance-preview.component.html",
+  styleUrls: ["./instance-preview.component.scss"]
 })
 export class InstancePreviewComponent {
-    title = "instance-preview";
+  title = "instance-preview";
 
-    constructor(public instanceService: InstancePreviewService, public categoryService: CategoryService, public filterService: FilterService) {
-    }
+  constructor(public instanceService: InstancePreviewService, public categoryService: CategoryService, public filterService: FilterService) {
+  }
 
-    protected readonly toPairs = toPairs;
-    protected readonly valuesIn = valuesIn;
+  protected readonly toPairs = toPairs;
+  protected readonly valuesIn = valuesIn;
 
-    getItems(){
-        return this.instanceService.loadedInstances.filter(i => this.filterService.shouldDisplay(i));
-    }
+  getItems() {
+    return this.instanceService.loadedInstances.filter(i => this.filterService.shouldDisplay(i));
+  }
 
-    // :((
+  getCategoryLength() {
+    let categoryValue = this.categoryService.getCategoryValue();
+    if (!categoryValue) return 0;
+    return categoryValue.fields.length;
+  }
 }
 
