470a9d1b05ecfeedd72b3b52ece88a5aa326df94
KaiMittendorff <kmittendor@stud.hs-heilbronn.de>
2023-12-09 10:25:34 +0100

    fixed

--- frontend/src/app/instance-preview/instance-preview.component.spec.ts
+++ frontend/src/app/instance-preview/instance-preview.component.spec.ts
@@ -1,23 +1,48 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { InstancePreviewComponent } from "./instance-preview.component";
-import { getTestBedDeclarations, getTestBedImports } from "../testbed.app.module";
+import { getTestBedImports, getTestBedDeclarations} from "../testbed.app";
+import { dummyApplicationData} from "../mocks/fetch/applicationdummydata.spec";
+import { FetchMockSpec } from "../fetch.mock.spec";
+import { domUpdate, elementToBePresent } from "../helper.spec";
 
 describe("InstancePreviewComponent", () => {
   let component: InstancePreviewComponent;
   let fixture: ComponentFixture<InstancePreviewComponent>;
+  let interval: any;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       ...getTestBedImports(),
       ...getTestBedDeclarations()
     });
+    FetchMockSpec.getInstance().setSpy();
+    FetchMockSpec.getInstance().setResponseData(dummyApplicationData);
+
     fixture = TestBed.createComponent(InstancePreviewComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    interval = setInterval(() => fixture.detectChanges(), 2);
   });
+
+  afterEach(() => {
+    FetchMockSpec.getInstance().resetResponseData();
+    clearInterval(interval);
+  });
+
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should initialize with loaded instances", async () => {
+    await domUpdate(fixture);
+    let preview = await elementToBePresent("div.instance-preview-container", fixture) as HTMLElement;
+    debugger;
+    // expect the preview to exist
+    expect(preview).toBeTruthy();
+  });
+
+  it("should display correct instance name", async () => {
+
   });
 });
