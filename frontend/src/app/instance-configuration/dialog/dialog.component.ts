0abe2876bcb1e7c848b71b88ab184ea73d427ced
Lennart Petrick <lpetrick@stud.hs-heilbronn.de>
2023-12-15 11:35:55 +0100

    changed dialog size to 80%.

--- frontend/src/app/instance-configuration/dialog/dialog.component.ts
+++ frontend/src/app/instance-configuration/dialog/dialog.component.ts
@@ -23,9 +23,8 @@ export class DialogComponent {
     return new Promise<MatDialogRef<InstanceConfigurationComponent, any>>((resolve) => {
       const dialogRef = this.dialog.open(InstanceConfigurationComponent, {
         data: { instance: this.instance },
-        panelClass: 'custom-dialog-container',
-        height: '80%',
-        width: '80%'
+        width: "80%",
+        height: "80%",
       });
       this.dialogRef = dialogRef;
 
