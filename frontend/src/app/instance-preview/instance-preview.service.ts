724f41c1d70a8792f8e4c83af52ea452018777b3
KaiMittendorff <kmittendor@stud.hs-heilbronn.de>
2024-01-10 15:33:08 +0100

    started documentation on instance preview component and HTML

--- frontend/src/app/instance-preview/instance-preview.service.ts
+++ frontend/src/app/instance-preview/instance-preview.service.ts
@@ -1,30 +1,34 @@
-import {APIService} from "../api.service";
-import {Injectable} from "@angular/core";
-import {CategoryService} from "../category/category.service";
-import {Category} from "../category/models/Category";
-import {InstanceComparison} from "../models/instance-comparison";
+import { APIService } from "../api.service";
+import { Injectable } from "@angular/core";
+import { CategoryService } from "../category/category.service";
+import { Category } from "../category/models/Category";
+import { InstanceComparison } from "../models/instance-comparison";
 
 
-
+ // This service is responsible for loading and storing the instances that are displayed in the instance preview.
+ // It is also responsible for loading the instances from the backend and detecting changes in the category.
 @Injectable({
-    providedIn: "root"
+  providedIn: "root"
 })
+
 export class InstancePreviewService {
-    title = "instance-preview service";
-    loadedInstances: InstanceComparison[] = [];
+  title = "instance-preview service";
+  loadedInstances: InstanceComparison[] = [];
 
-    constructor(public api: APIService, public categoryService: CategoryService) {
-        //@ts-ignore
-        window["InstancePreviewService"] = this;
-        this.categoryService.getCategory().subscribe((category) => this.categoryChanged(category));
-    }
+  // This method loads the instances from the backend and stores them in the loadedInstances array.
+  constructor(public api: APIService, public categoryService: CategoryService) {
+    //@ts-ignore
+    window["InstancePreviewService"] = this;
+    this.categoryService.getCategory().subscribe((category) => this.categoryChanged(category));
+  }
 
-    private async categoryChanged(category: Category | null) {
-        if (category == null) {
-            return;
-        }
-        this.loadedInstances = await this.api.loadInstances(category.name);
+  // This method returns the loaded instances if the category is changed.
+  private async categoryChanged(category: Category | null) {
+    if (category == null) {
+      return;
     }
+    this.loadedInstances = await this.api.loadInstances(category.name);
+  }
 
 }
 
