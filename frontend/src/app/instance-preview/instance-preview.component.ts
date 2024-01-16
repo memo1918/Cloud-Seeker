9f571bfad99030e1e732f12e4529c9ce4bbe2d2d
KaiMittendorff <kmittendor@stud.hs-heilbronn.de>
2024-01-16 16:24:32 +0100

    finished documentation for instance preview

--- frontend/src/app/instance-preview/instance-preview.component.ts
+++ frontend/src/app/instance-preview/instance-preview.component.ts
@@ -4,7 +4,8 @@ import { toPairs, valuesIn } from "lodash";
 import { CategoryService } from "../category/category.service";
 import { FilterService } from "../filter/filter.service";
 
-
+// This component is responsible for displaying the instances in the instance preview.
+// It also contains the logic for displaying the instances.
 @Component({
   selector: "app-instance-preview",
   templateUrl: "./instance-preview.component.html",
@@ -12,17 +13,17 @@ import { FilterService } from "../filter/filter.service";
 })
 export class InstancePreviewComponent {
   title = "instance-preview";
-
   constructor(public instanceService: InstancePreviewService, public categoryService: CategoryService, public filterService: FilterService) {
   }
-
   protected readonly toPairs = toPairs;
   protected readonly valuesIn = valuesIn;
 
+  // This method returns the instances that are displayed in the instance preview.
   getItems() {
     return this.instanceService.loadedInstances.filter(i => this.filterService.shouldDisplay(i));
   }
 
+  // This method returns the length of the category.
   getCategoryLength() {
     let categoryValue = this.categoryService.getCategoryValue();
     if (!categoryValue) return 0;
